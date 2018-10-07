e15d51ca5eb5280991a6285a15d90bbe93500d85
Ezequiel <ezequiel.salatino@gmail.com>
2018-10-07 11:26:54 -0300

    Home mobile wording & projects transitions

--- salaweb/src/Containers/Home/index.js
+++ salaweb/src/Containers/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Responsive from 'react-responsive';
 import { Link } from 'react-router-dom';
 
 
@@ -20,12 +21,20 @@ class Home extends React.Component {
   }
 
   render() {
-
+    const Desktop = props => <Responsive {...props} minWidth={1280} />;
+    const Mobile = props => <Responsive {...props} maxWidth={1279} />;
     return (
       <div className={"home " + this.state.statusClass}>
         <div className="container">
           <div className="col col-12">
-            <h1 className="   1">Hi, I’m Ezequiel Salatino. <span className="highlight">UI Designer</span>, living in Mendoza, Argentina.</h1>
+            <Desktop>
+              <h1 className="   1">Hi, I’m Ezequiel Salatino. <span className="highlight">UI Designer</span>, living in Mendoza, Argentina.</h1>
+            </Desktop>
+
+            <Mobile>
+              <h1 className="   1">Hello,<br/> I’m Ezequiel Salatino.<span className="highlight">UI Designer</span> from Argentina.</h1>
+            </Mobile>
+
             <p className="p-b ">
               <Link to="/info/">
                 About me
